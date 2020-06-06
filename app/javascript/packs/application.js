d4daed3ab6a8214c4820baf64d9d449b13c57c50
torrent benoit <torrentdu64@gmail.com>
2020-06-06 17:37:35 +1200

    push to heroku

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -31,4 +31,46 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+      var textWrapper = document.querySelector(".intro-title");
+    textWrapper.innerHTML = textWrapper.textContent.replace(
+        /([^\x00-\x80]|\w)/g,
+        "<span class='letter'>$&</span>"
+    );
+
+    anime
+        .timeline({ loop: false })
+        .add({
+            targets: ".intro-title .letter",
+            translateX: [140, 0],
+            translateZ: 0,
+            opacity: [0, 1],
+            easing: "easeOutExpo",
+            duration: 1400,
+            delay: function(el, i) {
+                return 500 + 50 * i;
+            }
+        })
+        .add({
+            targets: ".intro-title .letter",
+            translateX: [0, -140],
+            opacity: [1, 0],
+            easing: "easeInExpo",
+            duration: 1200,
+            delay: function(el, i) {
+                return 700 + 50 * i;
+            }
+        });
+
+    TweenMax.to(".loader", 2.2, {
+        delay: 5,
+        top: "-100%",
+        ease: Expo.easeInOut
+    });
+
+    TweenMax.from(".brand-logo", 2, {
+        delay: 6,
+        y: 10,
+        opacity: 0,
+        ease: Expo.easeInOut
+    });
 });
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get '/frontend', to: 'pages#frontend'
+  get '/backend', to: 'pages#backend'
+  get '/hobbie', to: 'pages#hobbie'
+  get '/social', to: 'pages#social'
+  get '/project', to: 'pages#project'
+  get '/profile', to: 'pages#profile'
+  get '/experience', to: 'pages#experience'
+  get '/language', to: 'pages#language'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
